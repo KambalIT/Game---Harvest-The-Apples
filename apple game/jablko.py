ebdfc9346240ee124178db520c3b8aa4768194fc
Kamil Balcerzak <89705806+KambalIT@users.noreply.github.com>
2023-04-20 21:07:19 +0200

    Delete apple game directory

--- apple game/jablko.py
+++
@@ -1,9 +0,0 @@
-# Kamil Balcerzak
-
-import pygame
-class Jablko: 
-    def __init__(self): # konstruktor 
-        self.obiekt = pygame.image.load('data/png/jablko.png')
-        self.pozycja = pygame.Vector2()
-
-        # self to zmienna globalna która gdy zostanie zdefiniowana staje się widoczna w każdej metodzie klasy
